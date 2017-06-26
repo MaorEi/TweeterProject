b77a94bbe72b4f3c6ea4778f02e57f50bbd06e9c
MaorEi <maor.eitan.me@gmail.com>
2017-06-27 01:38:52 +0300

    no finished

--- js/filters.js
+++ js/filters.js
@@ -1,14 +1,15 @@
 function filterByUsername(){
 	var input,filter,usersList,userCellNames,userCells;
-
 	input = document.getElementById("user-filter-input");
-	filter = input.value.toUpperCase();
-	usersList = document.getElementById("users-list");
-	userCells = usersList.getElementsByClassName("user-cell");
-	for (var i = 0; i < userCells.length; i++) {
+    filter = input.value.toUpperCase();
+    usersList = document.getElementById("users-list");
+    userCells = usersList.getElementsByClassName("user-cell");
+    console.log(userCells);
+    for (var i = 0; i < userCells.length; i++) {
 		//console.log(userCellName[i].innerHTML.toUpperCase());
 		userCellNames = userCells[i].getElementsByClassName("user-cell-name");
 		for (var j = 0; j < userCellNames.length; j++) {
+			console.log(userCellNames[j]);
 			if(userCellNames[j].innerHTML.toUpperCase().startsWith(filter)) 
 				userCells[i].style.display = "";
 				//userCells[i].classList.remove("hide-me");
